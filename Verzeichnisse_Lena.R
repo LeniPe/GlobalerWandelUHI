88efdc9fe6d07e257b561be5d74f2cc2e1d71d73
Lena Perzlmaier <45842617+LeniPe@users.noreply.github.com>
2021-02-15 12:29:05 +0100

    Create Markdown

--- Verzeichnisse_Lena.R
+++ Verzeichnisse_Lena.R
@@ -7,3 +7,14 @@ lstdir <- "C:/Users/Lena/Documents/Uni_Marburg/WS_2021/Globaler Wandel/Lokaler_T
 clcdir <- "C:/Users/Lena/Documents/Uni_Marburg/WS_2021/Globaler Wandel/Lokaler_Teil/CLC/reclass"
 
 outdir <- 'C:/Users/Lena/Documents/Uni_Marburg/WS_2021/Globaler Wandel/Lokaler_Teil/UHI_Marburg_Ergebnisse/2000/'
+
+
+## Verzeichnisse PC Jonas
+
+
+shapedir <- 'G:/Studium_Geographie/MSc_Physische_Geographie/Kurse/Globaler Wandel/Stadtklima in Marburg/Lokaler_Teil/Verwaltungdsebenen_GADM'
+shapefile <- '/Marburg_Stadtgebiet.shp'
+lstdir <- "G:/Studium_Geographie/MSc_Physische_Geographie/Kurse/Globaler Wandel/Stadtklima in Marburg/Lokaler_Teil/LST_Daten/2000"
+clcdir <- "G:/Studium_Geographie/MSc_Physische_Geographie/Kurse/Globaler Wandel/Stadtklima in Marburg/Lokaler_Teil/CLC/reclass"
+
+outdir <- 'G:/Studium_Geographie/MSc_Physische_Geographie/Kurse/Globaler Wandel/Stadtklima in Marburg/Lokaler_Teil/UHI_Marburg_Ergebnisse/2000/'
